7ffe7661f0af263b91883fe6cc389c5a4680bfad
Artem Timchenko <artem.timchenko@wishabi.com>
2018-08-28 12:32:51 +0300

    Modified

--- python_app/hello.py
+++ python_app/hello.py
@@ -1,17 +1,16 @@
-from flask import Flask
+from flask import *
+from db_test import connect
 import socket
 import uuid
 
 app = Flask(__name__)
+indent = "    "
 
-
-@app.route("/")
-def hello():
+@app.route('/', methods = ['GET', 'POST', 'DELETE'])
+def hello_world():
     host_ip = socket.gethostbyname(socket.gethostname())
     host_mac = hex(uuid.getnode())
-    return str(host_ip) + " And the mac is: " + host_mac
+    return str(host_ip) + " And the mac is: " + host_mac + "     " + connect()
 
-
-
-if __name__ == "__main__":
-    app.run(port=5010)
+if __name__ == '__main__':
+    app.run()
